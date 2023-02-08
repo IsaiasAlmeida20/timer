bafcdf495b408e3a1cd5ef4081f3d1fc635eff66
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-02-08 10:12:06 -0300

    :recycle: Refactor disabled inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -108,6 +108,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            disabled={!!activeCycle}
             {...register('task')}
           />
 
@@ -126,6 +127,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
